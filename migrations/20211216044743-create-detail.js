ee8e910150e7708801d8e6d2eb08ae25769cda65
rafi-ramdhani <rafiramdhani20@gmail.com>
2021-12-16 12:06:46 +0700

    Migrated

---
+++ migrations/20211216044743-create-detail.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Details', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      directorName: {
+        type: Sequelize.STRING
+      },
+      yearReleased: {
+        type: Sequelize.STRING
+      },
+      rating: {
+        type: Sequelize.DECIMAL
+      },
+      sinopsis: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Details');
+  }
+};
